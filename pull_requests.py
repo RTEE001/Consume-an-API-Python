0ca562c6c23916a0c5216f27358e99fef404329b
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-06-29 23:09:30 +0200

    Update pull_requests.py

--- pull_requests.py
+++ pull_requests.py
@@ -1,25 +1,32 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv
 
 def get_pull_requests(owner, repo_name, start_date, end_date):
-
+  
+    load_dotenv()  
+    TOKEN = os.getenv("TOKEN")
     url = f"https://api.github.com/repos/{owner}/{repo_name}/pulls?state=all"
-    url_response = requests.get(url, auth=({owner}, "password"))
+    url_response = requests.get(url, auth=({owner}, TOKEN))
     repos = url_response.json()
     pull_details = []
 
     if "message" in repos:
-        if repos["message"] == "Not Found":
+            print(repos["message"])
             raise Exception("Error 404 User or Repo Not Found")
     else:
-        another_page = True
+        another_page= True
         while another_page:
             json_data = json.dumps(repos, indent=4)
             python_data = json.loads(json_data)
-
+            
             for each_object in python_data:
                 if start_date <= each_object.get("created_at")[:10] <= end_date:
-                    pull_details.append(
+                    if each_object.get("id") in [item["id"] for item in pull_details]:
+                        pass
+                    else:
+                        pull_details.append(
                         {
                             "id": each_object.get("id"),
                             "user": each_object.get("user")["login"],
@@ -28,11 +35,10 @@ def get_pull_requests(owner, repo_name, start_date, end_date):
                             "created_at": each_object.get("created_at")[:10],
                         }
                     )
-
-            if "next" in url_response.links:
+                        
+            if "next" in url_response.links.keys():         
                 url_response = requests.get(url_response.links["next"]["url"])
                 repos.extend(url_response.json())
             else:
                 another_page = False
-
     return pull_details
