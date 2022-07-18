068e8f88ca6e3d8976068d1ef600c84ded33bbcd
Thifhidzi Rammbuda <70268186+RTEE001@users.noreply.github.com>
2022-07-18 03:51:48 +0200

    removed unnecessary if statement

--- pull_requests.py
+++ pull_requests.py
@@ -39,46 +39,46 @@ def check_repo(owner, repo_name):
 
 def get_pull_requests(owner, repo_name, start_date, end_date):
 
-    if check_repo(owner, repo_name):
+    check_repo(owner, repo_name)
 
-        url = f"https://api.github.com/repos/{owner}/{repo_name}/pulls?state=all"
-        url_response = requests.get(url, auth=({owner}, get_token()))
-        repos = url_response.json()
-        pull_details = []
+    url = f"https://api.github.com/repos/{owner}/{repo_name}/pulls?state=all"
+    url_response = requests.get(url, auth=({owner}, get_token()))
+    repos = url_response.json()
+    pull_details = []
 
-        while True:
-            for each_object in repos:
-                if (
-                    each_object.get("created_at") != None
-                    and start_date <= each_object.get("created_at")[:10] <= end_date
-                    or (
-                        each_object.get("merged_at") != None
-                        and start_date <= each_object.get("merged_at")[:10] <= end_date
-                    )
-                    or (
-                        each_object.get("updated_at") != None
-                        and start_date <= each_object.get("updated_at")[:10] <= end_date
-                    )
-                    or (
-                        each_object.get("closed_at") != None
-                        and start_date <= each_object.get("closed_at")[:10] <= end_date
-                    )
-                ):
+    while True:
+        for each_object in repos:
+            if (
+                each_object.get("created_at") != None
+                and start_date <= each_object.get("created_at")[:10] <= end_date
+                or (
+                    each_object.get("merged_at") != None
+                    and start_date <= each_object.get("merged_at")[:10] <= end_date
+                )
+                or (
+                    each_object.get("updated_at") != None
+                    and start_date <= each_object.get("updated_at")[:10] <= end_date
+                )
+                or (
+                    each_object.get("closed_at") != None
+                    and start_date <= each_object.get("closed_at")[:10] <= end_date
+                )
+            ):
 
-                    pull_details.append(
-                        {
-                            "id": each_object.get("id"),
-                            "user": each_object.get("user")["login"],
-                            "title": each_object.get("title"),
-                            "state": each_object.get("state"),
-                            "created_at": each_object.get("created_at")[:10],
-                        }
-                    )
+                pull_details.append(
+                    {
+                        "id": each_object.get("id"),
+                        "user": each_object.get("user")["login"],
+                        "title": each_object.get("title"),
+                        "state": each_object.get("state"),
+                        "created_at": each_object.get("created_at")[:10],
+                    }
+                )
 
-            if "next" not in url_response.links:
-                break
-            else:
-                url_response = requests.get(url_response.links["next"]["url"])
-                repos = url_response.json()
+        if "next" not in url_response.links:
+            break
+        else:
+            url_response = requests.get(url_response.links["next"]["url"])
+            repos = url_response.json()
 
     return pull_details
